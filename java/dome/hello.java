f9ef586be1ec19ba2bf54b11adf7fef14dd3e1c3
liuhaixiao <mk9540@163.com>
2022-05-24 14:28:38 +0800

    init

---
+++ java/dome/hello.java
@@ -0,0 +1,7 @@
+
+public class Hello {
+    /* This is my first java program.
+    * This will print 'Hello World' as the output */
+    public static void main(String []args) { System.out.println("Hello World"); 
+    } 
+}
